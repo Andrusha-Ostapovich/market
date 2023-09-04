3a229ef6c10069c75db0a2c76309950975aedb27
Андрій Остапович <andrii.ostapovich@gmail.com>
2023-09-04 10:13:26 +0300

    UpdateNewDay

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -3,17 +3,17 @@
 @section('content')
 
 @include('admin.parts.content-header', [
-    'page_title' => 'Користувачі',
+    'page_title' => 'Товари',
     'url_back' => route('products.index'),
 ])
 
     <section class="content">
         <div class="card">
             <div class="card-header">
-                <h3 class="card-title">Редагувати {{  $products->title }}</h3>
+                <h3 class="card-title">Редагувати {{  $products->name }}</h3>
             </div>
             <div class="card-body">
-            {!! Lte3::formOpen(['action' => route('products.update', $products->id), 'model' => $products, 'method' => 'PUT']) !!}
+            {!! Lte3::formOpen(['action' => route('products.update', $products), 'model' => $products, 'method' => 'PUT']) !!}
     @include('admin.products.inc.form', ['products' => $products])
             {!! Lte3::formClose() !!}
             </div>
